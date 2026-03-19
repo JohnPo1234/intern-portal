1b60cd69b205b1c375f835274f76f7ed29189615
JohnPo1234 <jhanikposnanski@gmail.com>
2026-03-19 10:00:02 -0500

    added Google Form link

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -5,7 +5,7 @@ export default function Questions() {
         <h1>Ask a Question</h1>
 
         <iframe
-          src="PASTE_GOOGLE_FORM_LINK"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdjmTwXy3dyGhoWi8sM0fS2ak3qNWKd9eKyznMeqQDoeMwg7Q/viewform?usp=sharing&ouid=105062683961735307727"
           width="100%"
           height="700"
           style={{ border: "none" }}
